c499f1928d29847f83c04799ffcbcfbf6871187d
careless-coder95 <dhananjaymaurya3000@gmail.com>
2026-03-05 23:47:31 +0530

    Add files via upload

---
+++ config (1).py
@@ -0,0 +1,6 @@
+
+
+# Pyrogram setup
+API_ID = "12345678"  # Replace this API ID with your actual API ID
+API_HASH = "XXXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "XXXXXXX:XXXXXXXX"  # Replace this BOT_TOKEN
